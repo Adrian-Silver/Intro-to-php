e7f2a422e89d5eaf0e1fa97ef111cbe5d4c952e6
Adrian-Silver <adrianallangitau@gmail.com>
2020-02-04 11:34:51 +0300

    started php

---
+++ Lesson1/lesson1.php
@@ -0,0 +1,39 @@
+<?php
+//Introduction to Php
+//PHP is a widely-used, open source scripting language
+//PHP scripts are executed on the server
+//PHP files can contain text HTML, CSS, JavaScript, and PHP code
+//PHP code is executed int he server, and the result is returned to the browser as plain HTML
+//PHP files have extension ".php"
+
+echo "Hello world";
+echo"<h1>Hello world</h1>";
+//echo:means print content in browser
+//Hello world :is the content
+//; :end of php statement or instruction
+
+//VARIABLES IN PHP
+//a container for storing data
+$myname ="John Doe <br>";
+echo $myname;
+$comp1 ="Kenya Bureau of Statistics <br>";
+echo $comp1;
+$comp2 ="Tesla <br>";
+echo $comp2;
+$comp3 ="Sony <br>";
+echo $comp3;
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aperiam atque dicta, ducimus eos esse et ipsa laborum laudantium, nostrum, odio odit provident repellendus voluptas!</p>
+
