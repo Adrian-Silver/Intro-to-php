5826572b4152dde11b11aa2ac0fd3dc8d7374730
Adrian-Silver <adrianallangitau@gmail.com>
2020-02-20 12:23:53 +0300

    users and posts

---
+++ Blogpoint/add_post.php
@@ -0,0 +1,107 @@
+<?php
+require 'config.php';
+require 'header.php';
+$user_id=$title=$content='';
+
+//grab user id
+if(isset($_SESSION['kipande'])){
+    $user_id = $_SESSION['kipande'];
+    $title = $_POST['title'];
+    $content= $_POST['content'];
+
+    echo "$title,$content";
+
+    if (!isset($user_id)){
+        header("location:login.php");
+    }
+    if (!isset($title)){
+        header("location:add_post.php");
+    }
+    if (!isset($content)){}
+
+
+//    add post into db
+    $sql="INSERT INTO `posts`(`id`, `user_id`, `title`, `post_content`, `created_at`) VALUES (NULL,'$user_id','$title','$content',NULL )";
+    if (mysqli_query($connection,$sql)){
+
+    }else{
+        echo "ERROR:" .mysqli_error($connection);
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+?>
+
+<!--start post form-->
+<div class="container">
+    <div class="row">
+        <div class="col-md-3 col-lg-3 col-xl-3"></div>
+        <div class="col-md-6 col-lg-6 col-xl-6">
+            <div id="auth-form">
+                <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>" method="post">
+                    <fieldset>
+
+                        <div class="form-group">
+                            <label for="">Title</label>
+                            <input type="text" name="title" class="form-control">
+                            <input type="number" name="id" hidden value="<?php echo $user_id?>">
+                        </div>
+
+                        <div class="form-group">
+                            <textarea name="content" type="text"></textarea>
+                        </div>
+
+
+
+                        <div class="form-group">
+                            <button class="btn btn-info btn-block" name="btn_post">Add Post</button>
+                        </div>
+
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+        <div class="col-md-3 col-lg-3 col-xl-3"></div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+<!--end post form-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+require 'footer.php';
+?>
---
+++ Blogpoint/footer.php
@@ -0,0 +1,7 @@
+<!--    links to js-->
+<!--    start with jquery-->
+<script src="static/js/jquery-3.4.1.js"></script>
+<script src="static/js/bootstrap.min.js"></script>
+</body>
+</html>
+
